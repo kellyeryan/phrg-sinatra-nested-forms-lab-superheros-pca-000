09a7b3fe55fcb2aaa116e41418f624d4a2f8ac4a
kellyeryan <kellyeryan@gmail.com>
2019-10-21 14:22:38 -0400

    Finish sinatra superheros lab

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,25 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get "/" do
 
+      erb :super_hero
+    end
+
+    post "/" do
+
+      @team_name = params[:team][:name]
+      @team_motto = params[:team][:motto]
+      @members = params[:team][:members]
+      @hero_name = []
+      @hero_power = []
+      @hero_bio = []
+      @members.each do |h|
+        @hero_name << h[:name]
+        @hero_power << h[:power]
+        @hero_bio << h[:bio]
+      end
+
+      erb :team
+    end
 end
